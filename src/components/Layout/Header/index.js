34fed2f87ee19ff1708b5e4328a8984837cc7d71
Anano Topuria <anano.topuriaa@gmail.com>
2024-11-01 02:21:12 +0400

    feat: add header content

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,6 +1,38 @@
+import Navigation from "./Navigation";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaInstagram,
+  FaTiktok,
+  FaYoutube,
+  FaGoogle,
+} from "react-icons/fa";
+
+import logo from "./../../../assets/images/LOGO.png";
+
 function Header() {
-  
-    return <h1>Header</h1>;
-  }
-  
-  export default Header;
+  return (
+    <header>
+      <div>
+        <p>
+          BTU AI4Globe საქართველოდან მთელს მსოფლიოში, AI წიგნიერების
+          გასაძლიერებლად
+        </p>
+        <div>
+          <FaFacebook />
+          <FaLinkedin />
+          <FaInstagram />
+          <FaTiktok />
+          <FaYoutube />
+          <FaGoogle />
+        </div>
+      </div>
+      <div>
+        <img src={logo} alt="btu ai logo" />
+        <Navigation />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
