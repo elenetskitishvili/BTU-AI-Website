0aa4de1c56fcbb091bf9b2842e9fa67c5af1f77a
Anano Topuria <anano.topuriaa@gmail.com>
2024-11-03 01:51:03 +0400

    feat: add general styles file, header styling, and responsive burger menu

--- src/components/Layout/Header/Navigation/index.js
+++ src/components/Layout/Header/Navigation/index.js
@@ -1,19 +1,71 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import {
+  Nav,
+  NavList,
+  NavItem,
+  StyledLink,
+  BurgerMenu,
+  MobileNav,
+} from "./Navigation.styles";
 
 function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/aboutus">ჩვენ შესახებ</Link>
-          <Link to="/bachelorinai">AI ბაკალავრის პროგრამა</Link>
-          <Link to="/studentsuccessstories">წარმატების ისტორიები</Link>
-          <Link to="/contactus">კონტაქტი</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/blog">ბლოგი</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <Nav>
+        <NavList>
+          <NavItem>
+            <StyledLink to="/aboutus">ჩვენ შესახებ</StyledLink>
+          </NavItem>
+          <NavItem>
+            <StyledLink to="/bachelorinai">AI ბაკალავრის პროგრამა</StyledLink>
+          </NavItem>
+          <NavItem>
+            <StyledLink to="/studentsuccessstories">წარმატების ისტორიები</StyledLink>
+          </NavItem>
+          <NavItem>
+            <StyledLink to="/contactus">კონტაქტი</StyledLink>
+          </NavItem>
+          <NavItem>
+            <StyledLink to="/faq">FAQ</StyledLink>
+          </NavItem>
+          <NavItem>
+            <StyledLink to="/blog">ბლოგი</StyledLink>
+          </NavItem>
+        </NavList>
+      </Nav>
+
+      <BurgerMenu onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </BurgerMenu>
+
+      <MobileNav isOpen={isOpen}>
+        <ul>
+          <li>
+            <StyledLink to="/aboutus" onClick={toggleMenu}>ჩვენ შესახებ</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/bachelorinai" onClick={toggleMenu}>AI ბაკალავრის პროგრამა</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/studentsuccessstories" onClick={toggleMenu}>წარმატების ისტორიები</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/contactus" onClick={toggleMenu}>კონტაქტი</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/faq" onClick={toggleMenu}>FAQ</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/blog" onClick={toggleMenu}>ბლოგი</StyledLink>
+          </li>
+        </ul>
+      </MobileNav>
+    </>
   );
 }
 
--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -7,31 +7,45 @@ import {
   FaYoutube,
   FaGoogle,
 } from "react-icons/fa";
-
 import logo from "./../../../assets/images/LOGO.png";
+import {
+  HeaderContainer,
+  TopBar,
+  TopText,
+  Highlight,
+  TopContainer,
+  SocialIcons,
+  HeaderWrapper,
+  LogoContainer,
+  LogoImage,
+} from "./Header.styles";
 
 function Header() {
   return (
-    <header>
-      <div>
-        <p>
-          BTU AI4Globe საქართველოდან მთელს მსოფლიოში, AI წიგნიერების
-          გასაძლიერებლად
-        </p>
-        <div>
-          <FaFacebook />
-          <FaLinkedin />
-          <FaInstagram />
-          <FaTiktok />
-          <FaYoutube />
-          <FaGoogle />
-        </div>
-      </div>
-      <div>
-        <img src={logo} alt="btu ai logo" />
-        <Navigation />
-      </div>
-    </header>
+    <HeaderContainer>
+      <TopBar>
+        <TopContainer>
+          <TopText>
+            <Highlight>BTU AI4Globe</Highlight> საქართველოდან მთელს მსოფლიოში,{" "}
+            <Highlight>AI</Highlight> წიგნიერების გასაძლიერებლად
+          </TopText>
+          <SocialIcons>
+            <FaFacebook />
+            <FaLinkedin />
+            <FaInstagram />
+            <FaTiktok />
+            <FaYoutube />
+            <FaGoogle />
+          </SocialIcons>
+        </TopContainer>
+      </TopBar>
+      <LogoContainer>
+        <HeaderWrapper>
+          <LogoImage src={logo} alt="btu ai logo" />
+          <Navigation />
+        </HeaderWrapper>
+      </LogoContainer>
+    </HeaderContainer>
   );
 }
 
