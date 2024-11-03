7f1aa009ba63ba32867932f91d7732c771a9eb7b
l0kokin <sali.gogishvili1@gmail.com>
2024-11-04 00:08:28 +0400

    add programs section. add h1 styles globally. add responsive font-size for global html

--- src/components/HomePageComponents/Programs/index.js
+++ src/components/HomePageComponents/Programs/index.js
@@ -1,7 +1,46 @@
+import ProgramCard from "./ProgramCard";
+import { ProgramsContainer, ProgramsList } from "./styles";
+import AI from "../../../assets/images/womenai.png";
+import USAID from "../../../assets/images/usaid.png";
+import UKR from "../../../assets/images/ukr.png";
+import MENTOR from "../../../assets/images/mentorship.png";
+
 function Programs() {
-  return <div>
-    <h1>Programs</h1>
-  </div>;
+  return (
+    <ProgramsContainer>
+      <h1>ქალების გაძლიერების პროგრამები</h1>
+      <ProgramsList>
+        <ProgramCard
+          img={USAID}
+          text={
+            "1500 ქალის გადამზადება ტექნოლოგიებში – უმსხვილესი პროექტი BTU-ში USAID-თან პარტნიორობით ხორციელდება;"
+          }
+          color={"#FC4B58"}
+        />
+        <ProgramCard
+          img={AI}
+          text={
+            "Women in AI-ის ფარგლებში, მთელი საქართველოს მასშტაბით 200 ქალი შეირჩა და  9 თვის განმავლობაში ხელოვნურ ინტელექტისა და ვებ-დეველოპმენტის მიმართულებებით გადამზადა."
+          }
+          color={"#070B80"}
+        />
+        <ProgramCard
+          img={UKR}
+          text={
+            "საქართველოში მცხოვრები უკრაინელი ქალები ტექნოლოგიებს შეისწავლიან. პროექტს-BTU, გაეროს ქალთა ორგანიზაციისა და იაპონიის მთავრობის მხარდაჭერით განახორციელებს."
+          }
+          color={"#FD007D"}
+        />
+        <ProgramCard
+          img={MENTOR}
+          text={
+            "ევროკავშირის მიერ მხარდაჭერილი პროექტი „ქალების მენტორობა ტექ პროგრამებში’’ მიზნად ისახავს, სამი ნაკადის განმავლობაში, 1100 ბენეფიციარისა და 320 მენტორის მოზიდვას."
+          }
+          color={"#000046"}
+        />
+      </ProgramsList>
+    </ProgramsContainer>
+  );
 }
 
 export default Programs;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -8,12 +8,45 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     margin: 0;
 }
+
 html{
     font-size: 62.5%;
+
+    /* 1440px - 9px */
+    @media (max-width: 90em) {
+        font-size: 56.25%;
+    }
+
+    /* 1200px - 8px */
+    @media (max-width: 75em) {
+        font-size:50%;
+    }
+
+    /* 800px - 7px */
+    @media (max-width: 50em) {
+        font-size:43.75%;
+    }
+
+    /* 400px - 6px */
+    @media (max-width: 25em) {
+        font-size:37.5%;
+    }
 }
 
 body{
     font-family: "Noto Sans Georgian", sans-serif;
+}
+
+h1{
+    font-size: 2.4rem;
+    font-weight: 700;
+    line-height: 3.26rem;
+    text-align: left;
+    width: 38.8rem;
+    white-space: nowrap;
+    border-bottom: 4px solid #FD007D;
+    padding-bottom: 1.3rem;
+    margin-bottom: 7.3rem;
 }
 `;
 
