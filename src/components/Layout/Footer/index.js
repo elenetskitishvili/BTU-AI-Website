6605390b4eb9c4948f0ea936f1a7d54cb27674a9
eli <ebezhiasvhili@gmail.com>
2024-11-08 01:27:26 +0400

    add footer styles

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -2,30 +2,39 @@ import React from "react";
 import logo from "./../../../assets/images/LOGO2.png";
 import {
   FooterContainer,
-  LogoImage,
   CopyrightContainer,
   Wrapper,
   Text,
   ListItem,
+  ContactUsContainer,
 } from "./Footer.styles";
+
+import ContactUsComponents from "../../ContactUsComponents";
 
 function Footer() {
   return (
     <FooterContainer>
       <Wrapper>
         <img src={logo} alt="btu ai logo" />
-        <Text>
-          ბიზნესისა და ტექნოლოგიების უნივერსიტეტი 2016 წლის 16 სექტემბერს
-          დაარსდა.
-        </Text>
-        <ul>
-          <ListItem>თბილისი 0162, საქართველო ი.ჭავჭავაძის გამზირი N82</ListItem>
-          <ListItem>(+995 32) 2 195 015</ListItem>
-        </ul>
+        <div>
+          <Text>
+            ბიზნესისა და ტექნოლოგიების უნივერსიტეტი 2016 წლის 16 სექტემბერს
+            დაარსდა.
+          </Text>
+          <ul>
+            <ListItem>
+              თბილისი 0162, საქართველო ი.ჭავჭავაძის გამზირი N82
+            </ListItem>
+            <ListItem>(+995 32) 2 195 015</ListItem>
+          </ul>
+        </div>
       </Wrapper>
       <CopyrightContainer>
-        <h1>copyright</h1>
+        <span>Copyright © 2024 - BTUSTUDENTS</span>
       </CopyrightContainer>
+      <ContactUsContainer>
+        <ContactUsComponents />
+      </ContactUsContainer>
     </FooterContainer>
   );
 }
