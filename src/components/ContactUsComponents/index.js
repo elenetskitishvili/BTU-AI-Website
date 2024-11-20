2719490ef58770e22af0d5e3ccf6135cc8c1e743
eli <ebezhiasvhili@gmail.com>
2024-11-20 15:27:45 +0400

    move contactform from contactus to footer

--- src/components/ContactUsComponents/index.js
+++ src/components/ContactUsComponents/index.js
@@ -1,29 +1,9 @@
 import React from "react";
-import arrow from "./../../assets/images/Frame.png";
-import {
-  ContactUsContainer,
-  Form,
-  Label,
-  InputContainer,
-  Input,
-  Icon,
-  Button,
-} from "./ContactUs.styles";
+// import arrow from "./../../assets/images/Frame.png";
+// import {} from "./ContactUs.styles";
 
 function ContactUsComponents() {
-  return (
-    <ContactUsContainer>
-      <Form>
-        <Label>მოგვწერე</Label>
-        <InputContainer>
-          <Input placeholder="Your Email" />
-          <Button type="submit">
-            <Icon src={arrow} alt="arrow icon" />
-          </Button>{" "}
-        </InputContainer>
-      </Form>
-    </ContactUsContainer>
-  );
+  return <></>;
 }
 
 export default ContactUsComponents;
--- src/components/Layout/Footer/Footer.styles.js
+++ src/components/Layout/Footer/Footer.styles.js
@@ -4,7 +4,7 @@ export const FooterContainer = styled.footer`
   background-color: #474747;
   display: flex;
   flex-direction: column;
-  position: relative;
+  position: absolute;
   bottom: 0;
   left: 0;
   right: 0;
@@ -51,7 +51,7 @@ export const CopyrightContainer = styled.div`
   }
 `;
 
-export const ContactUsContainer = styled.div`
+export const ContactUsWrapper = styled.div`
   position: absolute;
   bottom: 0;
   right: 0;
@@ -64,4 +64,58 @@ export const ContactUsContainer = styled.div`
     top: 5%;
     right: 55%;
   }
+`;
+export const ContactUsContainer = styled.div`
+  width: 539px;
+  height: 382px;
+  background-color: #fd007d;
+  border-top-left-radius: 40px;
+  @media (max-width: 85em) {
+    width: 0px;
+    height: 0px;
+  }
+`;
+
+export const Form = styled.form`
+  padding: 179px 0 0 47px;
+  @media (max-width: 85em) {
+    padding: 0;
+  }
+`;
+
+export const Label = styled.label`
+  font-weight: 700;
+  font-size: 13px;
+  color: #ffffff;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  width: 144px;
+  height: 21px;
+  border-radius: 30px;
+  border: none;
+  padding-left: 11px;
+  &::placeholder {
+    color: #4747476e;
+    font-weight: 700;
+  }
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
+
+export const Button = styled.button`
+  background: transparent;
+  border: none;
+`;
+
+export const Icon = styled.img`
+  position: relative;
+  right: 11px;
 `;
