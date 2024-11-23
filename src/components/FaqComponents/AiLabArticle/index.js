a919784c03dbec07c09857ead70ba28efe98556a
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-11-23 17:08:24 +0400

    refactor: refactoring AI Lab page for backend implementation

--- src/components/FaqComponents/AiLabArticle/index.js
+++ src/components/FaqComponents/AiLabArticle/index.js
@@ -1,3 +1,160 @@
+// import {
+//   ArticleContent,
+//   ArticleText,
+//   ArticleLink,
+//   ArticleList,
+//   ArticleListItem,
+// } from "./AiLabArticleStyles";
+
+// export default function AiLabArticle() {
+//   return (
+//     <ArticleContent>
+//       <ArticleText>
+//         BTU AI LAB - ხელოვნური ინტელექტის პირველი რეგიონალური ლაბორატორია. BTU-ს
+//         სტუდენტებს და ნებისმიერ დაინტერესებულ ადამიანს აქვს საშუალება მსოფლიოს
+//         წამყვან IT კომპანიებთან ითანამშრომლონ და მათთვის საინტერესო პროდუქტი
+//         განავითარონ.BTU AI LAB თანამშრომლობს ხელოვნური ინტელექტის
+//         ბიზნესასოციაცია
+//         <ArticleLink
+//           href="https://www.aigeorgia.ge/"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           AI Georgia-სთან,
+//         </ArticleLink>
+//         რომელიც აქტიურად მუშაობს ქვეყანაში ხელოვნური ინტელექტის ეროვნული
+//         სტრატეგიის შექმნის, AI-ს ქვეყნის სტრატეგიულ სექტორებში და
+//         ბიზნესინდუსტრიებში ინტეგრაციის, სამეცნიერო კვლევების მხარდაჭერის და
+//         ინტერნაციონალიზაციის მიმართულებით.
+//       </ArticleText>
+//       <ArticleText>
+//         ბიზნესისა და ტექნოლოგიების უნივერსიტეტის AI LAB-ის ბაზაზე მომზადდა:
+//       </ArticleText>
+//       <ArticleList>
+//         <ArticleListItem>
+//           BTU-ს სტუდენტის მიერ დაარსებული კომპანია SYSTEM CORP-ის მიერ
+//           <ArticleLink
+//             href="https://www.youtube.com/playlist?list=PLLReDJtJiSt_YVw4_VK5QKmyYw-4ZSRkM"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             ავატარის ვიდეო ლექციები:
+//           </ArticleLink>
+//           მეწარმეობის, ტექნოლოგიებისა და ინოვაციების მიმართულებით.
+//         </ArticleListItem>
+//       </ArticleList>
+//       <ArticleText>BTU AI LAB - ის ბაზაზე ჩატარდა:</ArticleText>
+//       <ArticleList>
+//         <ArticleListItem>
+//           <ArticleLink
+//             href="https://btu.edu.ge/btu-ai-lab-is-gadamzadebis-kursebi-sachuqrad-da-qveqhnis-ganvitharebisthvis-mnishvnelovani-proeqtebi/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             ხელოვნური ინტელექტის მიმართულებით გადამზადების კურსი საჩუქრად
+//             ქალებისთვის
+//           </ArticleLink>
+//           - ბიზნესისა და ტექნოლოგიების უნივერსიტეტის ინიციატივითა და აშშ-ის
+//           საელჩოს მხარდაჭერით.
+//         </ArticleListItem>
+//         <ArticleListItem>
+//           <ArticleLink
+//             href="https://btu.edu.ge/btu-ai-lab-is-gadamzadebis-kursebi-sachuqrad-da-qveqhnis-ganvitharebisthvis-mnishvnelovani-proeqtebi/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             მონაცემთა ანალიტიკის მიმართულებით გადამზადების პროგრამა,
+//           </ArticleLink>
+//           ყველა ბენეფიციარისთვის, სრულად დაფინანსებული ბიზნესისა და
+//           ტექნოლოგიების უნივერსიტეტის მიერ.
+//         </ArticleListItem>
+//         <ArticleListItem>
+//           <ArticleLink
+//             href="https://btu.edu.ge/btu-ai-lab-is-gadamzadebis-kursebi-sachuqrad-da-qveqhnis-ganvitharebisthvis-mnishvnelovani-proeqtebi/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             ხელოვნური ინტელექტის მიმართულებით გადამზადება, პროგრამაში მონაწილე
+//             ყველა ბენეფიციარისთვის,
+//           </ArticleLink>
+//           სრულად დაფინანსებული ბიზნესისა და ტექნოლოგიების უნივერსიტეტის მიერ
+//         </ArticleListItem>
+//         <ArticleListItem>
+//           „ბიზნესისა და ტექნოლოგიების უნივერსიტეტმა“ AI LAB-ის ბაზაზე კიდევ ერთი
+//           პროექტის განხორციელება გერმანულ კომპანია KnowledgeTools International
+//           GmbH-სა და Maxin AI-თან ერთად დაიწყო,
+//           <ArticleLink
+//             href="https://btu.edu.ge/btu-ai-lab-germanuli-knowledgetools-international-gmbh-isthvis-ai-gadatsqhvetilebas-qmnis/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             რაც ქართული შრიფტისთვის სრულყოფილ OCR პროგრამის შექმნაზე
+//           </ArticleLink>
+//           მუშაობას გულისხმობს, რომლის შედეგადაც დასკანერებულ დოკუმენტებში
+//           შეცდომის გარეშე მოხდება ტექსტის წაკითხვა. პროექტი ბიზნესისა და
+//           ტექნოლოგიების უნივერსიტეტის AI LAB-ის დახმარებითა და უშუალო
+//           მონაწილეობით განხორციელდება.
+//         </ArticleListItem>
+//         <ArticleListItem>
+//           გაეროს ხელოვნური ინტელექტის და რობოტიქსის ცენტრის ხელმძღვანელის
+//           <ArticleLink
+//             href="https://btu.edu.ge/rogor-inergeba-khelovnuri-inteleqti-globalurad-da-ra-momavali-aqvs-mas-irakli-beridzis-masterklasi-btu-shi/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             მასტერკლასი
+//           </ArticleLink>
+//           და პანელური დისკუსია ციფრული მმართველობის შესახებ
+//         </ArticleListItem>
+//         <ArticleListItem>
+//           <ArticleLink
+//             href="https://www.entrepreneur.com/ka/siakhleebi-da-tendentsiebi/btu-shi-studenturi/443891"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Pulsar AI ჰაკათონი
+//           </ArticleLink>
+//           - ხელოვნური ინტელექტის მიმართულებით, BTU-ს სტუდენტებისთვის
+//         </ArticleListItem>
+//         <ArticleListItem>
+//           AI Book Series პრეზენტაცია -
+//           <ArticleLink
+//             href="https://www.marketer.ge/ai-marketing-the-future-of-customer-engagement-btu/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             AI Marketing: The Future of Customer Engagement
+//           </ArticleLink>
+//         </ArticleListItem>
+//         <ArticleListItem>
+//           პანელური დისკუსია Beyond Human:
+//           <ArticleLink
+//             href="https://www.facebook.com/BTUGEORGIA/posts/pfbid02QNd76k5d51a7FmBhyH34HaenGdL21GFUostHAminCWVAFqK5PSQSRBzQ4iXLXfy2l"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             The Advancements of ChatGPT and GPT-3
+//           </ArticleLink>
+//         </ArticleListItem>
+//         <ArticleListItem>
+//           უმსხვილესი სტუდენტური
+//           <ArticleLink
+//             href="https://www.interpressnews.ge/ka/article/788195-btu-m-umsxvilesi-studenturi-ai-hackathon-chaatara"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             AI ჰაკათონი
+//           </ArticleLink>
+//           - სადაც საქართველოს მასშტაბით, 22 უნივერსიტეტიდან სტუდენტებმა შექმნეს
+//           ხელოვნური ინტელექტის პროდუქტები/სერვისები, რომლებიც დააინტეგრირეს
+//           საგანმანათლებლო სექტორში. გამარჯვებულმა სტუდენტებმა 20 000 ლარიანი
+//           საპრიზო ფონდიდან მიიღეს ინვესტიციები.
+//         </ArticleListItem>
+//       </ArticleList>
+//     </ArticleContent>
+//   );
+// }
+import React, { useEffect, useState } from "react";
 import {
   ArticleContent,
   ArticleText,
@@ -7,13 +164,30 @@ import {
 } from "./AiLabArticleStyles";
 
 export default function AiLabArticle() {
+  const [articles, setArticles] = useState([]);
+
+  // Fetch data from the backend
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/faq-ailab/");
+        const data = await response.json();
+        setArticles(data);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
   return (
     <ArticleContent>
       <ArticleText>
         BTU AI LAB - ხელოვნური ინტელექტის პირველი რეგიონალური ლაბორატორია. BTU-ს
         სტუდენტებს და ნებისმიერ დაინტერესებულ ადამიანს აქვს საშუალება მსოფლიოს
         წამყვან IT კომპანიებთან ითანამშრომლონ და მათთვის საინტერესო პროდუქტი
-        განავითარონ.BTU AI LAB თანამშრომლობს ხელოვნური ინტელექტის
+        განავითარონ. BTU AI LAB თანამშრომლობს ხელოვნური ინტელექტის
         ბიზნესასოციაცია
         <ArticleLink
           href="https://www.aigeorgia.ge/"
@@ -45,111 +219,22 @@ export default function AiLabArticle() {
       </ArticleList>
       <ArticleText>BTU AI LAB - ის ბაზაზე ჩატარდა:</ArticleText>
       <ArticleList>
-        <ArticleListItem>
-          <ArticleLink
-            href="https://btu.edu.ge/btu-ai-lab-is-gadamzadebis-kursebi-sachuqrad-da-qveqhnis-ganvitharebisthvis-mnishvnelovani-proeqtebi/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ხელოვნური ინტელექტის მიმართულებით გადამზადების კურსი საჩუქრად
-            ქალებისთვის
-          </ArticleLink>
-          - ბიზნესისა და ტექნოლოგიების უნივერსიტეტის ინიციატივითა და აშშ-ის
-          საელჩოს მხარდაჭერით.
-        </ArticleListItem>
-        <ArticleListItem>
-          <ArticleLink
-            href="https://btu.edu.ge/btu-ai-lab-is-gadamzadebis-kursebi-sachuqrad-da-qveqhnis-ganvitharebisthvis-mnishvnelovani-proeqtebi/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            მონაცემთა ანალიტიკის მიმართულებით გადამზადების პროგრამა,
-          </ArticleLink>
-          ყველა ბენეფიციარისთვის, სრულად დაფინანსებული ბიზნესისა და
-          ტექნოლოგიების უნივერსიტეტის მიერ.
-        </ArticleListItem>
-        <ArticleListItem>
-          <ArticleLink
-            href="https://btu.edu.ge/btu-ai-lab-is-gadamzadebis-kursebi-sachuqrad-da-qveqhnis-ganvitharebisthvis-mnishvnelovani-proeqtebi/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ხელოვნური ინტელექტის მიმართულებით გადამზადება, პროგრამაში მონაწილე
-            ყველა ბენეფიციარისთვის,
-          </ArticleLink>
-          სრულად დაფინანსებული ბიზნესისა და ტექნოლოგიების უნივერსიტეტის მიერ
-        </ArticleListItem>
-        <ArticleListItem>
-          „ბიზნესისა და ტექნოლოგიების უნივერსიტეტმა“ AI LAB-ის ბაზაზე კიდევ ერთი
-          პროექტის განხორციელება გერმანულ კომპანია KnowledgeTools International
-          GmbH-სა და Maxin AI-თან ერთად დაიწყო,
-          <ArticleLink
-            href="https://btu.edu.ge/btu-ai-lab-germanuli-knowledgetools-international-gmbh-isthvis-ai-gadatsqhvetilebas-qmnis/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            რაც ქართული შრიფტისთვის სრულყოფილ OCR პროგრამის შექმნაზე
-          </ArticleLink>
-          მუშაობას გულისხმობს, რომლის შედეგადაც დასკანერებულ დოკუმენტებში
-          შეცდომის გარეშე მოხდება ტექსტის წაკითხვა. პროექტი ბიზნესისა და
-          ტექნოლოგიების უნივერსიტეტის AI LAB-ის დახმარებითა და უშუალო
-          მონაწილეობით განხორციელდება.
-        </ArticleListItem>
-        <ArticleListItem>
-          გაეროს ხელოვნური ინტელექტის და რობოტიქსის ცენტრის ხელმძღვანელის
-          <ArticleLink
-            href="https://btu.edu.ge/rogor-inergeba-khelovnuri-inteleqti-globalurad-da-ra-momavali-aqvs-mas-irakli-beridzis-masterklasi-btu-shi/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            მასტერკლასი
-          </ArticleLink>
-          და პანელური დისკუსია ციფრული მმართველობის შესახებ
-        </ArticleListItem>
-        <ArticleListItem>
-          <ArticleLink
-            href="https://www.entrepreneur.com/ka/siakhleebi-da-tendentsiebi/btu-shi-studenturi/443891"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Pulsar AI ჰაკათონი
-          </ArticleLink>
-          - ხელოვნური ინტელექტის მიმართულებით, BTU-ს სტუდენტებისთვის
-        </ArticleListItem>
-        <ArticleListItem>
-          AI Book Series პრეზენტაცია -
-          <ArticleLink
-            href="https://www.marketer.ge/ai-marketing-the-future-of-customer-engagement-btu/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            AI Marketing: The Future of Customer Engagement
-          </ArticleLink>
-        </ArticleListItem>
-        <ArticleListItem>
-          პანელური დისკუსია Beyond Human:
-          <ArticleLink
-            href="https://www.facebook.com/BTUGEORGIA/posts/pfbid02QNd76k5d51a7FmBhyH34HaenGdL21GFUostHAminCWVAFqK5PSQSRBzQ4iXLXfy2l"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            The Advancements of ChatGPT and GPT-3
-          </ArticleLink>
-        </ArticleListItem>
-        <ArticleListItem>
-          უმსხვილესი სტუდენტური
-          <ArticleLink
-            href="https://www.interpressnews.ge/ka/article/788195-btu-m-umsxvilesi-studenturi-ai-hackathon-chaatara"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            AI ჰაკათონი
-          </ArticleLink>
-          - სადაც საქართველოს მასშტაბით, 22 უნივერსიტეტიდან სტუდენტებმა შექმნეს
-          ხელოვნური ინტელექტის პროდუქტები/სერვისები, რომლებიც დააინტეგრირეს
-          საგანმანათლებლო სექტორში. გამარჯვებულმა სტუდენტებმა 20 000 ლარიანი
-          საპრიზო ფონდიდან მიიღეს ინვესტიციები.
-        </ArticleListItem>
+        {articles.map((article) => (
+          <ArticleListItem key={article.id}>
+            {article.link ? (
+              <ArticleLink
+                href={article.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {article.title}
+              </ArticleLink>
+            ) : (
+              article.title
+            )}
+            {article.description && ` - ${article.description}`}
+          </ArticleListItem>
+        ))}
       </ArticleList>
     </ArticleContent>
   );
