075522393fe9202fee8a069ed01a1168c866c171
lost-in-noise <nancykikvadze@gmail.com>
2024-11-23 16:51:47 +0400

    add  page title and heading

--- src/components/FaqComponents/index.js
+++ src/components/FaqComponents/index.js
@@ -1,9 +1,11 @@
-// import PageTitleSection from "./../ReusableComponents/PageTitleSection"
+import PageTitleSection from "./../ReusableComponents/PageTitleSection"
+import SectionHeading from "./../ReusableComponents/SectionHeading"
 
 function FaqComponents() {
   return (
     <div>
-      {/* <PageTitleSection title="ხშირად დასმული კითხვები" /> */}
+      <PageTitleSection title="ხშირად დასმული კითხვები" />
+      <SectionHeading heading="FAQ" />
     </div>
   );
 }
