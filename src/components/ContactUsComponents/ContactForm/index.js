9d6640243164eb6ec373d9ac122d37ceccab04e4
eli <ebezhiasvhili@gmail.com>
2024-11-25 15:32:37 +0400

    change url:

--- src/components/ContactUsComponents/ContactForm/index.js
+++ src/components/ContactUsComponents/ContactForm/index.js
@@ -58,13 +58,16 @@ function ContactForm() {
       setError("");
       setSuccessMessage("");
 
-      const response = await fetch("http://127.0.0.1:8000/contactus/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formValues),
-      });
+      const response = await fetch(
+        "https://wai-django-final-b9968118d906.herokuapp.com/contactus/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formValues),
+        }
+      );
 
       if (!response.ok) {
         const errorData = await response.text();
